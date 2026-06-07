bd3074929b22d7f786f7c27146c7d946b0a399df
rahulnshah <rahulnshah3012@gmail.com>
2026-06-07 09:30:34 -0400

    added purchase order row

---
+++ src/purchase-order-row/purchase-order-row.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-order-row',
+  imports: [],
+  templateUrl: './purchase-order-row.html',
+  styleUrl: './purchase-order-row.css',
+})
+export class PurchaseOrderRow {
+
+}
